96f59e0a06cf73b57cb506da1440c75b2d9318f8
Dennis Oe <93004899+DennisOe@users.noreply.github.com>
2022-04-05 23:45:55 +0200

    basic functionality tweaked

--- core/server.py
+++ core/server.py
@@ -1,33 +1,46 @@
-import paramiko
 import os
+import paramiko
 from settings import Settings
 
 
 class Server(Settings):
+    """This class manages interaction with remote server."""
     def __init__(self):
+        super().__init__()
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    def ssh_connect(self, state):
+    def ssh_connect(self, state: bool):
+        """Connect or disconnect ssh."""
         if state:
             if self.ping():
-                if self.ssh.get_transport() is None:
-                    self.ssh.connect(Settings.host, 22, Settings.user, Settings.password)
+                if not self.ssh_activ():
+                    self.ssh.connect(self.settings["host"], 22, self.settings["user"], self.settings["password"])
         else:
-            if self.ssh.get_transport() is not None:
-                if self.ssh.get_transport().is_active():
-                    self.ssh.close()
+            if self.ssh_activ():
+                self.ssh.close()
 
-    def ssh_command(self, command):
-        stdin, stdout, stderr = self.ssh.exec_command(command)
-        outlines = stdout.readlines()
-        output = "".join(outlines)
-        return output
+    def ssh_activ(self) -> bool:
+        """Check if ssh connection is established."""
+        if self.ssh.get_transport() is not None:
+            if self.ssh.get_transport().is_active():
+                return True
+            else:
+                return False
+        else:
+            return False
 
-    @staticmethod
-    def ping():
+    def ssh_command(self, command: str) -> str:
+        """Issues bash commands."""
+        if self.ssh_activ():
+            stdin, stdout, stderr = self.ssh.exec_command(command)
+            outlines = stdout.readlines()
+            output = "".join(outlines)
+            return output
+
+    def ping(self) -> bool:
         """Check if server is online."""
-        if not os.system("ping -c 1 " + Settings.host + " 2>&1 >/dev/null"):
+        if not os.system("ping -c 1 {host} 2>&1 >/dev/null".format_map(self.settings)):
             return True
         else:
             return False
